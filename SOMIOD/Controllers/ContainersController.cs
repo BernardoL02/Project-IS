d68deef4852c6d94e2cb4f9de79446a9eabb8ec2
Bernardo Lopes <bernaslopes0708.bl@gmail.com>
2024-11-26 11:15:07 +0000

    Fix bug

---
+++ SOMIOD/Controllers/ContainersController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOMIOD.Controllers
+{
+    public class ContainersController
+    {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ProjetoIS_D02.Properties.Settings.connStr"].ConnectionString;
+        
+    }
+}
